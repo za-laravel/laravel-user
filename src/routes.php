b7d2dafcaf0b31bf1b55125abfb4d25497b56e75
Serge Yourkiv <sergeyourkiv@gmail.com>
2015-05-27 17:48:14 +0300

    Add admin.user.block route

--- src/routes.php
+++ src/routes.php
@@ -5,3 +5,8 @@ use Illuminate\Support\Facades\Config;
 
 Route::model('user', Config::get('auth.model'));
 Route::resource('/admin/user', 'ZaLaravel\LaravelUser\Controllers\AdminUserController');
+
+Route::post('/admin/user/{user}/block', [
+    'uses' => 'ZaLaravel\LaravelUser\Controllers\AdminUserController@block',
+    'as' => 'admin.user.block',
+]);
