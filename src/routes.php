e0b42e246221e2a5714364e2c3cb29939a7a29a4
Serge Yourkiv <sergeyourkiv@gmail.com>
2015-05-27 17:11:32 +0300

    Add admin to route

--- src/routes.php
+++ src/routes.php
@@ -4,4 +4,4 @@ use Illuminate\Support\Facades\Route;
 use Illuminate\Support\Facades\Config;
 
 Route::model('user', Config::get('auth.model'));
-Route::resource('user', 'ZaLaravel\LaravelUser\Controllers\AdminUserController');
+Route::resource('/admin/user', 'ZaLaravel\LaravelUser\Controllers\AdminUserController');
